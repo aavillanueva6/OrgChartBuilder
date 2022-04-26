2bf894610a9b7ff904b9e6f3b56334e49d278191
Alejandro Villanueva <aavillanueva6@gmail.com>
2022-04-25 22:48:05 -0700

    builds inquirer logic

--- index.js
+++ index.js
@@ -1,1 +1,156 @@
+// call in required modules
 const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+// define global variable
+let orgArray = [];
+
+/**
+ * createManager function runs the inquirer prompts for the manager then calls the required functions after it is completed.
+ */
+function createManager() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: `What is your name?`,
+        name: 'empName',
+      },
+      {
+        type: 'input',
+        message: `What is your id?`,
+        name: 'empId',
+      },
+      {
+        type: 'input',
+        message: `What is your email address?`,
+        name: 'empEmail',
+      },
+      {
+        type: 'input',
+        message: `What is your office number?`,
+        name: 'empOffice',
+      },
+    ])
+    .then(function (response) {
+      const newMan = new Manager(
+        response.empName,
+        response.empId,
+        response.empEmail,
+        response.empOffice
+      );
+      orgArray.push(newMan);
+      promptAddAnother();
+    });
+}
+
+function promptAddAnother() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'Do you want to add another employee?',
+        name: 'anotherEmp',
+        choices: ['Yes', 'No'],
+      },
+    ])
+    .then(function (response) {
+      response.anotherEmp === 'Yes' ? promptEmpType() : buildHTML(orgArray);
+    });
+}
+
+function promptEmpType() {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        message: 'Is this employee and Engineer or and Intern?',
+        name: 'empType',
+        choices: ['Engineer', 'Intern'],
+      },
+    ])
+    .then(function (response) {
+      response.empType === 'Engineer' ? createEngineer() : createIntern();
+    });
+}
+
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: `What is the engineer's name?`,
+        name: 'empName',
+      },
+      {
+        type: 'input',
+        message: `What is the engineer's id?`,
+        name: 'empId',
+      },
+      {
+        type: 'input',
+        message: `What is the engineer's email address?`,
+        name: 'empEmail',
+      },
+      {
+        type: 'input',
+        message: `What is the engineer's github profile?`,
+        name: 'empGithub',
+      },
+    ])
+    .then(function (response) {
+      const newEng = new Engineer(
+        response.empName,
+        response.empId,
+        response.empEmail,
+        response.empGithub
+      );
+      orgArray.push(newEng);
+      promptAddAnother();
+    });
+}
+
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: 'input',
+        message: `What is the intern's name?`,
+        name: 'empName',
+      },
+      {
+        type: 'input',
+        message: `What is the intern's id?`,
+        name: 'empId',
+      },
+      {
+        type: 'input',
+        message: `What is the intern's email address?`,
+        name: 'empEmail',
+      },
+      {
+        type: 'input',
+        message: `What is the intern's school?`,
+        name: 'empSchool',
+      },
+    ])
+    .then(function (response) {
+      const newInt = new Intern(
+        response.empName,
+        response.empId,
+        response.empEmail,
+        response.empSchool
+      );
+      orgArray.push(newInt);
+      promptAddAnother();
+    });
+}
+
+function buildHTML(orgArray) {
+  console.log(orgArray);
+}
+
+// call the createManager function
+createManager();
